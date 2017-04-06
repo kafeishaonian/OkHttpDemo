714d3f8ce9643617973cbbc73fcc19752c7227fc
hongmingwei <648600445@qq.com>
2017-04-06 17:48:22 +0800

    新增Https请求，版本V1.1.0

--- app/src/main/java/com/okhttp/demo/MyApplication.java
+++ app/src/main/java/com/okhttp/demo/MyApplication.java
@@ -3,6 +3,7 @@ package com.okhttp.demo;
 import android.app.Application;
 
 
+import com.okhttp.demo.okhttp.utils.HttpUtils;
 import com.okhttp.demo.okhttp.utils.LogInterceptor;
 import com.okhttp.demo.okhttp.utils.OkHttpUtils;
 
@@ -18,10 +19,22 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        /**
+         * 设置Https请求，
+         * 证书的inputstream
+         * 本地证书的inputstream
+         * 本地证书的密码
+         */
+        HttpUtils.SSLParams sslParams = HttpUtils.getSslSocketFactory(null, null, null);
+
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .addInterceptor(new LogInterceptor("TAG"))
-                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
-                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                .addInterceptor(new LogInterceptor("TAG")) //log日志
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)//连接超时
+                .writeTimeout(10000L, TimeUnit.MILLISECONDS) //读取超时
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)  //写入超时
+                .pingInterval(10000L, TimeUnit.MILLISECONDS) //websocket 轮训间隔
+//                .sslSocketFactory(sslParams.sslSocketFactory, sslParams.trustManager)
                 .build();
 
         OkHttpUtils.initClient(okHttpClient);
